e0d7104402e01c443bd271e12d64570702cfc0e4
celtare21 <celtare21@gmail.com>
2019-01-25 09:01:08 +0000

    sdm845-common: roodir: Fix path
    Change-Id: I8dd7f45fa287ef9f593216813f316bd97535905d
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,5 +29,5 @@ LOCAL_MODULE       := init.performance.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.performance.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/init
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
